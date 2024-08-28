02ec55a75b582e82881ae7d55b8357c55e804402
pOsdas <ratmir1099@mail.ru>
2024-08-28 23:49:15 +0300

    add link checker

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def get_soup(url: str, domain: str) -> None:
     links = get_links(soup, domain)
     for link in links:
         print(link)
+    # check_links(links)
 
 
 def get_links(soup, domain) -> list:
@@ -26,6 +27,19 @@ def get_links(soup, domain) -> list:
         if domain in href or not domain:
             filtered_links.append(href)
     return filtered_links
+
+
+def check_links(links: list) -> None:
+    for link in links:
+        try:
+            response = requests.get(link)
+            if response.status_code == 200:
+                print(f"Available link: {link}")
+            else:
+                print(f"Unavailable link: {link} (Status code: {response.status_code})")
+
+        except requests.RequestException as e:
+            print(f"Error with {link}: {e}")
 
 
 if __name__ == "__main__":
