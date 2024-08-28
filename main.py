8af4b2e1739f457de56025ab73b8cb581b34a9b8
pOsdas <ratmir1099@mail.ru>
2024-08-28 23:09:50 +0300

    add domain filter

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
+import bs4
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_url():
+def get_url() -> None:
     url: str = input("Enter the url: ")
-    get_soup(url)
+    domain: str = input("Enter the domain to filter (leave empty for all links): ")
+    get_soup(url, domain)
 
 
-def get_soup(url):
+def get_soup(url: str, domain: str) -> None:
     response = requests.get(url)
     content = response.text
     soup = BeautifulSoup(content, 'lxml')
-    links = get_links(soup)
+    links = get_links(soup, domain)
     for link in links:
         print(link)
 
 
-def get_links(soup):
+def get_links(soup, domain) -> list:
     links = soup.find_all('a', href=True)
-    return [link['href'] for link in links]
+    filtered_links = []
+    for link in links:
+        href = link['href']
+        if domain in href or not domain:
+            filtered_links.append(href)
+    return filtered_links
 
 
 if __name__ == "__main__":
